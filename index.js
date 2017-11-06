49a1e3c381429a7c6476ca415a9fc50a4b18a0ce
Victoria <vitalina27@gmail.com>
2017-11-06 17:16:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ function theBeatlesPlay(musicians,instruments){
   let arr=[];
   for(let i=0;i<musicians.length;i++){
     arr.push(musicians+" plays "+instruments+"!!!");
+    console.log(arr);
   }
   return arr;
 }
