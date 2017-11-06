5c43d6ecde4a39ea836bcbbfa58a54b1947861eb
Victoria <vitalina27@gmail.com>
2017-11-06 17:45:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
 const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
 function theBeatlesPlay(musicians,instruments){
-  let arr=[];
-  for(let i=0;i<musicians.length;i++){
+  var arr=[];
+  for(var i=0;i<musicians.length;i++){
     arr.push(musicians[i]+" plays "+instruments[i]);
-    console.log(arr);
+  //  console.log(arr);
   }
   return arr;
 }
@@ -12,7 +12,7 @@ function johnLennonFacts(){
 var  arr=[  "He was the last Beatle to learn to drive",
     "He was never a vegetarian",
     "He was a choir boy and boy scout",
-    "He hated the sound of his own voice"]
+    "He hated the sound of his own voice"];
   var arr1=[];
   let len=0;
    while(len<arr.length){
