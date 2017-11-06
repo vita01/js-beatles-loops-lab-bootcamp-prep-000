9dc1a9d107e32a2052bb15af00eee1523a3f69a0
Victoria <vitalina27@gmail.com>
2017-11-06 17:55:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,40 +1,28 @@
-const musicians = ["John", "Paul", "George", "Ringo"];
-
-const instruments = ["guitar", "guitar", "bass", "drums"];
-
-function theBeatlesPlay(musicians, instruments) {
-  var array = [];
-  for (let i = 0; i < 4; i++) {
-      array.push(`${musicians[0]} plays ${instruments[0]}`);
+function theBeatlesPlay(players, instruments) {
+    var allPlayers = [];
+    for (var i=0; i < players.length; i++) {
+        allPlayers.push(players[i] + " plays " + instruments[i]);
     }
-  return array;
+    return allPlayers;
 }
 
-theBeatlesPlay(musicians, instruments);
-
-const facts = [
-  "He was the last Beatle to learn to drive",
-  "He was never a vegetarian",
-  "He was a choir boy and boy scout",
-  "He hated the sound of his own voice"
-];
-
-function johnLennonFacts(facts) {
-  var n=0;
-  while (n<facts.length) {
-    facts[n] + "!!!";
-    n++;
-  }
-  return facts;
+function johnLennonFacts() {
+    var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
+    var newFacts = [];
+    var i = 0;
+    while (i < facts.length) {
+        newFacts.push(facts[i] +"!!!");
+        i++;
+    }
+    return newFacts;
 }
 
-function iLoveTheBeatles(x) {
-  var array = [];
-  do {
-    x = array.push("I love the Beatles!");
-    x++;
-  } while (x < 15); {
-
-  }
-  return array;
+function iLoveTheBeatles(num) {
+    var love = [];
+    do {
+        love.push("I love the Beatles!");
+        num++;
+    } while (num < 15);
+    
+    return love;
 }
