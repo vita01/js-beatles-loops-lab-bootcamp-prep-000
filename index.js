e2add521cdf6e84dca4ea41fe44c4d7643c832f8
Victoria <vitalina27@gmail.com>
2017-11-06 16:38:45 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,10 @@
+const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
+const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
+function iLoveTheBeatles(musicians,instruments){
+  let arr=[];
+  for(let i=0;i<musician.length;i++){
+    arr.push(musicians+" plays "+instruments);
+  }
+  return arr;
+}
+
